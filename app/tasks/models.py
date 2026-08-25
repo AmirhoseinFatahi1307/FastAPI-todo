594b35aa1d70da1cf95beb9a97a3594b519f27ce
Amir <fatahiamirhosein8@gmail.com>
2026-08-25 20:05:41 +0330

    creating tasks table

---
+++ app/tasks/models.py
@@ -0,0 +1,16 @@
+from sqlalchemy import func, Column, String, Integer, Boolean, Text, DATETIME
+from core.database import Base
+
+
+class Task_models(Base):
+    __tablename__ = "tasks"
+    id = Column(Integer, primary_key=True, autoincrement=True)
+
+    title = Column(String(150), nullable=False)
+    description = Column(Text(500), nullable=True)
+    is_completed = Column(Boolean, default=False)
+
+    create_data = Column(DATETIME, server_default=func.now())
+    updated_data = Column(
+        DATETIME, server_default=func.now(), server_onupdate=func.now()
+    )
