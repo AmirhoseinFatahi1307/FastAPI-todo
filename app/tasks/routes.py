8317050fe6693ed51f82ae0210ef700e40c9bedf
Amir <fatahiamirhosein8@gmail.com>
2026-08-25 19:37:58 +0330

    creating routers

--- app/tasks/routes.py
+++ app/tasks/routes.py
@@ -1,8 +1,13 @@
 from fastapi import APIRouter
 
-router = APIRouter()
+router = APIRouter(tags=["Tasks"], prefix="/Todo")
 
 
-@router.get("/tasks")
-async def retrive_task_list():
+@router.get("/Tasks")
+async def retrieve_task_list():
+    return []
+
+
+@router.get("/Task/{Task_id}")
+async def retrieve_task_detail(Task_id: int):
     return []
