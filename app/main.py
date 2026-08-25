ee590d29b5eab8f8551868baa3c5b97992a0bb17
Amir <fatahiamirhosein8@gmail.com>
2026-08-25 19:37:31 +0330

    creating a api and adjusting router and metadata for api

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from tasks.routes import router
 
 
 @asynccontextmanager
@@ -9,4 +10,21 @@ async def lifespan(app=FastAPI):
     print("Application shutdown")
 
 
-app = FastAPI(lifespan=lifespan)
+metadata_tags = [
+    {"name": "Tasks", "description": "Operation related to task management"}
+]
+
+
+app = FastAPI(
+    title="Todo Application",
+    contact={
+        "name": "AmirhoseinFatahi1307",
+        "email": "fatahiamirhosein8@gmail.com",
+        "url": "https://www.linkedin.com/in/amirhosein-fatahi-64b797413/",
+    },
+    license_info={"name": "MIT"},
+    lifespan=lifespan,
+    openapi_tags=metadata_tags,
+)
+
+app.include_router(router)
