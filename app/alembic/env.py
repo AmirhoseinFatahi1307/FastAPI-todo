05e5125f954ea0c0821e90df15e47278e5d6bc57
Amir <fatahiamirhosein8@gmail.com>
2026-08-25 20:04:46 +0330

    with Path and os we direct alembic env and create base and import modules to the env and the directory of config that database url is in the

--- app/alembic/env.py
+++ app/alembic/env.py
@@ -1,9 +1,11 @@
+import os
 from logging.config import fileConfig
-
+from pathlib import Path
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-
+from core.database import Base
 from alembic import context
+from dotenv import load_dotenv
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -14,11 +16,24 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
+BASE_DIR = Path(__file__).resolve().parent.parent
+ENV_PATH = BASE_DIR / ".env"
+load_dotenv(ENV_PATH)
+DATABASE_URL = os.getenv("SQLALCHEMY_DATABASE_URL")
+config = context.config
+if DATABASE_URL:
+    config.set_main_option("sqlalchemy.url", DATABASE_URL)
+else:
+    raise ValueError("DATABASE_URL is not set in the environment variables")
+
+
 # add your model's MetaData object here
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+from tasks.models import *
+
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
@@ -64,9 +79,7 @@ def run_migrations_online() -> None:
     )
 
     with connectable.connect() as connection:
-        context.configure(
-            connection=connection, target_metadata=target_metadata
-        )
+        context.configure(connection=connection, target_metadata=target_metadata)
 
         with context.begin_transaction():
             context.run_migrations()
